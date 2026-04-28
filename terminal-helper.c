bee0277427a3903cfc34a9ccf0bcec3236678b61
DSAntonio08 <antonio.destefani08@gmail.com>
2026-04-28 11:40:59 +0200

    Selecting + TestPrinting

--- terminal-helper.c
+++ terminal-helper.c
@@ -1,9 +1,9 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h> // for flags
-#include <stdbool.h>
-#include <termios.h> // provides terminal interface for POSIX 
+#include <termios.h> // provides terminal interface for POSIX
+#include <unistd.h>  // for flags
 
 // return type:
 // 1 -> User Error
@@ -15,11 +15,12 @@
 #define PATH_SIZE 1024
 #define MATCH_LIMIT 128
 
-
 int num_of_matches = MATCH_LIMIT;
 int current_found = 0;
 bool search = false;
 int wantsHelp = true;
+bool isRawModeenabled = false;
+char *arrayofMatches[MATCH_LIMIT];
 
 struct termios original;
 
@@ -30,154 +31,224 @@ char *storeMatch(char *buffer);
 // need to switch to raw mode to "read keys"
 void enableRawMode();
 void disableRawMode();
+int readInput();
+void displaySelected(int selected, bool firstDraw);
 
 ///@param argc -> argument counter (how many arguments)
-///@param argv -> argument vector (what we are searching for) 
-int main(int argc, char * argv[])
-{
+///@param argv -> argument vector (what we are searching for)
+int main(int argc, char *argv[]) {
   history_remade(argc, argv);
+  enableRawMode();
+  readInput();
+  disableRawMode();
+
+  for (int i = 0; i < current_found; i++) {
+    free(arrayofMatches[i]);
+  }
+
   return 0;
 }
 
+int history_remade(int argumentCounter, char *argumentVector[]) {
+  int opt;
 
-int history_remade(int argumentCounter, char *argumentVector[])
-{
-    int opt;
-
-  while((opt = getopt(argumentCounter,argumentVector, ":sm:h")) != -1)
-  {
+  while ((opt = getopt(argumentCounter, argumentVector, ":sm:h")) != -1) {
     switch (opt) {
-      case 's':
-        search = true;
-        wantsHelp = false;
-        break;
-      case 'm':
-        // atoi converts ascii to int
-        // optarg is the argument/number that gets addet with the m Flag
-        num_of_matches = atoi(optarg); 
-
-        // for unreasonable stuff - default
-        if (num_of_matches > MATCH_LIMIT || num_of_matches < 1) {
-          num_of_matches = MATCH_LIMIT;
-        }
-        
-        wantsHelp = false;
-
-        // so that the s flag is Optional
-        search = true;
-        break;
-      case 'h':
-        wantsHelp = true;
-        break;
-      case ':':
-          fprintf(stderr,"Error: -m Needs a Number \n");
-          fprintf(stderr,"Use the following way: -m 10 OR -sm10\n");
-           
-          return 1;
+    case 's':
+      search = true;
+      wantsHelp = false;
       break;
-      default:
-        wantsHelp = true;
+    case 'm':
+      // atoi converts ascii to int
+      // optarg is the argument/number that gets addet with the m Flag
+      num_of_matches = atoi(optarg);
+
+      // for unreasonable stuff - default
+      if (num_of_matches > MATCH_LIMIT || num_of_matches < 1) {
+        num_of_matches = MATCH_LIMIT;
+      }
+
+      wantsHelp = false;
+
+      // so that the s flag is Optional
+      search = true;
+      break;
+    case 'h':
+      wantsHelp = true;
+      break;
+    case ':':
+      fprintf(stderr, "Error: -m Needs a Number \n");
+      fprintf(stderr, "Use the following way: -m 10 OR -sm10\n");
+
+      return 1;
+      break;
+    default:
+      wantsHelp = true;
+      break;
+    }
+  }
+
+  usageHelp(wantsHelp);
+  if (search) {
+    searchCommand(argumentCounter, argumentVector, optind);
+  }
+  return 0;
+}
+
+int searchCommand(int argc, char *argv[], int index) {
+  // Check if we have a keyword at the index getopt left us
+  if (index >= argc) {
+    fprintf(stderr, "No search term provided.\n");
+    return 1;
+  }
+
+  char *keyword = argv[index];
+  char *home = getenv("HOME");
+  if (!home)
+    return 3;
+
+  char path[PATH_SIZE];
+  snprintf(path, PATH_SIZE, "%s/.bash_history", home);
+
+  FILE *file_pointer = fopen(path, "r");
+  if (!file_pointer) {
+    fprintf(stderr, "Error: File not found.\n");
+    return 2;
+  }
+
+  char buffer[BUFFER_SIZE];
+
+  while (fgets(buffer, BUFFER_SIZE, file_pointer)) {
+    if (strstr(buffer, keyword)) {
+      arrayofMatches[current_found] = storeMatch(buffer);
+      current_found++;
+      if (current_found >= num_of_matches)
         break;
     }
   }
 
-
-  usageHelp(wantsHelp);
-
-  if(search){searchCommand(argumentCounter, argumentVector, optind);}
-  
-  return 0;
-}
-
-
-
-int searchCommand(int argc, char *argv[], int index) {
-    // Check if we have a keyword at the index getopt left us
-    if (index >= argc) {
-        fprintf(stderr, "No search term provided.\n");
-        return 1;
-    }
-
-    char *keyword = argv[index];
-    char *home = getenv("HOME");
-    if (!home) return 3;
-
-    char path[PATH_SIZE];
-    snprintf(path, PATH_SIZE, "%s/.bash_history", home);
-
-    FILE *file_pointer = fopen(path, "r");
-    if (!file_pointer) {
-        fprintf(stderr, "Error: File not found.\n");
-        return 2;
-    }
-
-    char *arrayofMatches[MATCH_LIMIT];
-    char buffer[BUFFER_SIZE];
-
-    while (fgets(buffer, BUFFER_SIZE, file_pointer)) {
-        if (strstr(buffer, keyword)) {
-            printf( "%s", buffer);
-            arrayofMatches[current_found] = storeMatch(buffer);
-            current_found++;
-            if (current_found >= num_of_matches) break;
-        }
-    }
-    for(int i = 0; i < current_found; i++)
-    {
-      free(arrayofMatches[i]);
-    }
-
-    // C has no garbage collector
-    fclose(file_pointer);
-
+  // C has no garbage collector
+  fclose(file_pointer);
 }
 
 void usageHelp(bool behelpful) {
-    if (behelpful) {
-        printf("\n"
-               "Usage: terminal-helper [FLAGS] <keyword>\n"
-               "\n"
-               "A lightweight utility to search and execute bash history.\n"
-               "\n"
-               "Flags:\n"
-               "  -s             Search mode (default if keyword is provided).\n"
-               "  -m <number>    Limit results to <number> of matches (Default: 128).\n"
-               "  -h             Display this help menu.\n"
-               "\n"
-               "Examples:\n"
-               "  ./terminal-helper -s nixos\n"
-               "  ./terminal-helper -m 5 gcc\n"
-               "  ./terminal-helper -sm 10 ls\n"
-               "\n");
-    }
+  if (behelpful) {
+    printf("\n"
+           "Usage: terminal-helper [FLAGS] <keyword>\n"
+           "\n"
+           "A lightweight utility to search and execute bash history.\n"
+           "\n"
+           "Flags:\n"
+           "  -s             Search mode (default if keyword is provided).\n"
+           "  -m <number>    Limit results to <number> of matches (Default: "
+           "128).\n"
+           "  -h             Display this help menu.\n"
+           "\n"
+           "Examples:\n"
+           "  ./terminal-helper -s nixos\n"
+           "  ./terminal-helper -m 5 gcc\n"
+           "  ./terminal-helper -sm 10 ls\n"
+           "\n");
+  }
 }
 
-char *storeMatch(char *buffer)
-{
+char *storeMatch(char *buffer) {
   // +1 for hidden "end of string" character
   int sizeofcommand = (strlen(buffer) + 1);
   char *foundCommand = malloc(sizeofcommand);
-  if(foundCommand == NULL)
+  if (foundCommand == NULL)
     return NULL;
   // stringcopy(destination, source)
-  strcpy(foundCommand, buffer); 
+  strcpy(foundCommand, buffer);
   return foundCommand;
 }
 
-void enableRawMode()
-{
+void enableRawMode() {
   struct termios orignal_copy;
   // get original config first
   // 0 = stdin
-  tcgettart(0, &original);
+  tcgetattr(0, &original);
   orignal_copy = original;
   // turn of these local flags
-  t.c_lflag &= ~(ICANON | ECHO);
-  tcsettart(0, TSCANOW, &orignal_copy);
-  
+  orignal_copy.c_lflag &= ~(ICANON | ECHO);
+  tcsetattr(0, TCSANOW, &orignal_copy);
+  isRawModeenabled = true;
 }
 
-void disableRawMode()
-{
-  tcsettart(0, TSCNOW, &original);
+void disableRawMode() {
+  tcsetattr(0, TCSANOW, &original);
+  isRawModeenabled = false;
+}
+
+int readInput() {
+  /*
+   q = quit
+   n = enter
+   Up arrow    → \x1b  [  A
+   Down arrow  → \x1b  [  B
+  */
+
+  if (isRawModeenabled) {
+    char character;
+    int selected = 0;
+    displaySelected(selected, true);
+
+    while (true) {
+      // 0 = stdin
+      // variable
+      // 1 = byte size
+      read(0, &character, 1);
+      if (character == '\x1b') {
+        // Arrow Key (maybe)
+        char sequence[2];
+        read(0, &sequence[0], 1); // should be [
+        read(0, &sequence[1], 1);
+
+        if (sequence[0] == '[') {
+          switch (sequence[1]) {
+          case 'A':
+            // go up
+            if (selected > 0) {
+              selected--;
+            }
+            break;
+          case 'B':
+            // go down
+            if (selected < current_found - 1) {
+              selected++;
+            }
+            break;
+          }
+        }
+      } else if (character == '\n') {
+        // Enter was pressed
+        printf("%s", arrayofMatches[selected]);
+        // then after executing/putting into terminal - quit
+        break;
+        break;
+      } else if (character == 'q') {
+        // quit
+        break;
+      }
+      displaySelected(selected, false);
+    }
+  } else {
+    fprintf(stderr, "Error: Terminal not in Raw Mode.\n");
+    return 3;
+  }
+}
+
+void displaySelected(int selected, bool firstDraw) {
+  if (!firstDraw) {
+    // move cursor to the top
+    for (int i = 0; i < current_found; i++) {
+      printf("\033[A\r"); // up one line, back to start of line
+    }
+  }
+  for (int i = 0; i < current_found; i++) {
+    if (i == selected)
+      printf("> %s", arrayofMatches[i]); // highlighted
+    else
+      printf("  %s", arrayofMatches[i]); // normal
+  }
 }
